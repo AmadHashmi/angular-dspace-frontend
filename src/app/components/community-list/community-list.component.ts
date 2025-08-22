b968f613b1ade36bdc7b46a6c412286b73e6e753
Amad Ul Hassan <ah@indigilabs.com>
2025-08-22 22:16:55 +0200

    listing communities

--- src/app/components/community-list/community-list.component.ts
+++ src/app/components/community-list/community-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DspaceService } from '../../services/dspace.service';
 
 @Component({
   selector: 'app-community-list',
   standalone: true,
   imports: [],
   templateUrl: './community-list.component.html',
-  styleUrl: './community-list.component.css'
+  styleUrl: './community-list.component.css',
 })
-export class CommunityListComponent {
+export class CommunityListComponent implements OnInit {
+  communities: any[] = [];
+  error: string | null = null;
+  loading = false;
+  constructor(private dspaceService: DspaceService) {}
+  ngOnInit(): void {
+    this.loadCommunities();
+  }
 
+  loadCommunities() {
+    this.loading = true;
+    this.dspaceService.getCommunities().subscribe({
+      next: (response) => {
+        this.communities = response._embedded.communities;
+        this.loading = false;
+      },
+      error: (error) => {
+        this.error = 'Failed to load communities';
+        this.loading = false;
+        console.error('Error', error);
+      },
+    });
+  }
 }
